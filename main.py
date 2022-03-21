78a1fe64e31f234ece84170ae4ae8b12f065635b
yangboyee <ryan.yangpoyi@gmail.com>
2022-03-21 23:40:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import disccord
+import discord
 from discord.ext import commands
 
-bot=commands.Bot(command_prefix']')
+bot=commands.Bot(command_prefix=']')
 
 @bot.event
+async def on_ready():
+    print('Zhan has landed')
 
-async def
+bot.run('OTU1MzM3OTgyMzMwNjc5MzU3.YjgN1A.LSCa879iWGkFoQ3SLwKGHl7OE_I')
