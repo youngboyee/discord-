3efe48a02d10adedd73a791ca814173a6f4f4059
youngboyee <84242774+youngboyee@users.noreply.github.com>
2022-03-23 22:28:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import discord
 from discord.ext import commands
 
-bot=commands.Bot(command_prefix=']')
+
+intents=discord.Intents.default()
+intents.members=True
+
+bot=commands.Bot(command_prefix='!',intents=intents)
 
 @bot.event
 async def on_ready():
     print('Zhan has landed')
 
-bot.run('OTU1MzM3OTgyMzMwNjc5MzU3.YjgN1A.LSCa879iWGkFoQ3SLwKGHl7OE_I')
+
+
+@bot.event
+async def on_member_join(member):
+    channel=bot.get_channel(955340986979086408)
+    await channel.send(f'{member}降落啦!')
+@bot.event
+async def on_member_remove(member):
+    channel=bot.get_channel(955340986979086408)
+    await channel.send(f'{member}飛走了...')
+
+
+bot.run('')
