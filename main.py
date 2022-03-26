df10e59bd7c43644412bb5477c040d58516bc4aa
yangboyee <ryan.yangpoyi@gmail.com>
2022-03-26 23:00:19 +0800

    hi

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
 import discord
 from discord.ext import commands
+import json
 
-bot=commands.Bot(command_prefix=']')
+intents=discord.Intents.default()
+intents.members=True
+
+bot=commands.Bot(command_prefix='!',intents=intents)
 
 @bot.event
 async def on_ready():
     print('Zhan has landed')
+    channel=bot.get_channel(955340986979086408)
+    await channel.send('Zhan降落啦!')
+    
 
-bot.run('OTU1MzM3OTgyMzMwNjc5MzU3.YjgN1A.LSCa879iWGkFoQ3SLwKGHl7OE_I')
+@bot.event
+async def on_member_join(member):
+    channel=bot.get_channel(955340986979086408)
+    await channel.send(f'{member}降落啦!')
+    
+@bot.event
+async def on_member_remove(member):
+    channel=bot.get_channel(955340986979086408)
+    await channel.send(f'{member}飛走了...')
+   
+@bot.command()
+async def ping(ctx):
+    await ctx.send('{0}毫秒'.format(round(bot.latency*1000)))
+
+
+bot.run('OTU1MzM3OTgyMzMwNjc5MzU3.YjgN1A.ocbuUmqhj4HRHwlFFGqq9aw2ga0')
