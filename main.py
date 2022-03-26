ab69e383bd582b1073311b72ee151e3a0f59888a
youngboyee <84242774+youngboyee@users.noreply.github.com>
2022-03-26 22:57:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,17 +10,23 @@ bot=commands.Bot(command_prefix='!',intents=intents)
 @bot.event
 async def on_ready():
     print('Zhan has landed')
-
-
+    channel=bot.get_channel(955340986979086408)
+    await channel.send('Zhan降落啦!')
+    
 
 @bot.event
 async def on_member_join(member):
     channel=bot.get_channel(955340986979086408)
     await channel.send(f'{member}降落啦!')
+    
 @bot.event
 async def on_member_remove(member):
     channel=bot.get_channel(955340986979086408)
     await channel.send(f'{member}飛走了...')
+   
+@bot.command()
+async def ping(ctx):
+    await ctx.send('{0}毫秒'.format(round(bot.latency*1000)))
 
 
 bot.run('')
