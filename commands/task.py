a63358ee85a6947624146bf50a20010aebe02d40
yangboyee <ryan.yangpoyi@gmail.com>
2022-04-03 11:55:05 +0800

    1

--- commands/task.py
+++ commands/task.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import json
+import asyncio
 import random
 import datetime as dt
 import pytz
@@ -10,12 +11,19 @@ with open('setting.json',mode='r',encoding='utf8') as jfile:
     jdata=json.load(jfile)
 
 class task(cog_extension):
-   def __init__(self,*args,**kwargs):
-       super().__init__(*args,**kwargs)
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
 
-       async def interval():
-           await self.bot.wait_until_ready()
-           while not self.bot.is_closed():
-               
-def setup(bot):
+        async def interval():#間格執行
+            await self.bot.wait_until_ready()
+            self.channel=self.bot.get_channel(848189292467191860)
+            while not self.bot.is_closed():#bot沒關就會一直執行
+                await self.channel.send('I am running')
+                await asyncio.sleep(5)#5秒
+        self.Atask=self.bot.loop.create_task(interval())
+@commands.command()
+async def setchannel(self,ctx,ch_id:int):
+    self.channel=self.bot.get_channel(ch_id)
+    await ctx.send('Set channel:{0}'.format(self.channel.mention))
+def setup(bot):                                          #tag他
     bot.add_cog(task(bot))
