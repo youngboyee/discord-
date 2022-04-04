e4adc35ebc3d57b05a3ddf26524578423ae9e8f4
yangboyee <ryan.yangpoyi@gmail.com>
2022-04-04 21:20:15 +0800

    Update react.py

--- commands/react.py
+++ commands/react.py
@@ -47,27 +47,29 @@ class react(cog_extension):
         city_name = city
         complete_url = jdata['base_url'] + "appid=" + jdata['api_key'] + "&q=" + city_name
         response = requests.get(complete_url)
-        self.x = response.json()
-        self.channel = ctx.message.channel
-        if self.x["cod"] != "404":
-            async with self.channel.typing():
-                self.y = self.x["main"]
-                temperature=self.y["temp"]
-                self.celsiuis_temperature= str(round(temperature-273.15))
-                self.pressure=self.y["pressure"]
-                self.humidity=self.y["humidity"]
-                self.z=self.x["weather"]
-                self.weather_description = self.z[0]["description"]
-                weather_description = self.z[0]["description"]
+        x = response.json()
+        channel = ctx.message.channel
+        if x["cod"] != "404":
+            async with channel.typing():
+                y = x["main"]
+                temperature=y["temp"]
+                celsiuis_temperature= str(round(temperature-273.15))
+                pressure=y["pressure"]
+                humidity=y["humidity"]
+                z=x["weather"]
+                weather_description = z[0]["description"]
+                weather_description = z[0]["description"]
                 embed = discord.Embed(title=f"Weather in {city_name}",
                                 color=ctx.guild.me.top_role.color,
                                 timestamp=ctx.message.created_at,)
                 embed.add_field(name="Descripition", value=f"**{weather_description}**", inline=False)
-                embed.add_field(name="溫度(C)", value=f"**{self.celsiuis_temperature}°C**", inline=False)
-                embed.add_field(name="濕度(%)", value=f"**{self.humidity}%**", inline=False)
-                embed.add_field(name="Atmospheric Pressure(hPa)", value=f"**{self.pressure}hPa**", inline=False)
+                embed.add_field(name="溫度(C)", value=f"**{celsiuis_temperature}°C**", inline=False)
+                embed.add_field(name="濕度(%)", value=f"**{humidity}%**", inline=False)
+                embed.add_field(name="Atmospheric Pressure(hPa)", value=f"**{pressure}hPa**", inline=False)
                 embed.set_thumbnail(url="https://i.ibb.co/CMrsxdX/weather.png")
                 embed.set_footer(text=f"Requested by {ctx.author.name}")
-
+                await channel.send(embed=embed)
+        else:
+            await channel.send("City not found.")
 def setup(bot):
     bot.add_cog(react(bot))
